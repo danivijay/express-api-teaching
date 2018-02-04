de259279462217862c565d24f626045231ae5dfa
Dani Vijay <danimvijay@gmail.com>
2018-02-04 22:14:06 +0530

    get req

--- index.js
+++ index.js
@@ -2,6 +2,11 @@ const express = require('express')
 
 const app = express()
 
+app.get('/', (req, res) => {
+  console.log('get req')
+  res.send({ name: 'Dani'})
+})
+
 app.listen(process.env.port || '4000', () => {
   console.log('listening in 4000')
 })

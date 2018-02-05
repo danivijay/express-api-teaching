07c77653f474849c8e52210766a7aba833f8aa38
Dani Vijay <danimvijay@gmail.com>
2018-02-05 21:07:04 +0530

    delete req

--- routes/api.js
+++ routes/api.js
@@ -8,7 +8,6 @@ router.get('/ninjas', (req, res, next) => {
 })
 
 router.post('/ninjas', (req, res, next) => {
-  console.log(req.body)
   Ninja.create(req.body).then(ninja => {
     res.send(ninja)
   }).catch(next)
@@ -19,7 +18,9 @@ router.put('/ninjas/:id', (req, res, next) => {
 })
 
 router.delete('/ninjas/:id', (req, res, next) => {
-  res.send({type: 'DELETE'})
+  Ninja.findByIdAndRemove(_id = req.params.id)
+    .then(ninja => res.send(ninja))
+    .catch(next)
 })
 
 module.exports = router
